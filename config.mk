f0a7469ff6771f77cfe769acbeb0ba8ae26ac87c
AdarshGrewal <adarshgrewal@gmail.com>
2021-11-20 18:38:04 +0530

    pixel: Import SetupWizard properties
    * Taken from the Pixel 8 Pro
    
    Signed-off-by: basamaryan <basam.aryan@gmail.com>
    Change-Id: If2eebc804a2637f974449c67e70bbe4576ced0e5

--- config.mk
+++ config.mk
@@ -46,6 +46,22 @@ PRODUCT_PACKAGES += \
     DevicePersonalizationPrebuiltPixel2020 \
     TurboAdapter
 
+# SetupWizard
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.setupwizard.enterprise_mode=1 \
+    ro.setupwizard.esim_cid_ignore=00000001 \
+    setupwizard.feature.baseline_setupwizard_enabled=true \
+    setupwizard.feature.day_night_mode_enabled=true \
+    setupwizard.feature.portal_notification=true \
+    setupwizard.feature.enable_quick_start_flow=true \
+    setupwizard.feature.enable_wifi_tracker \
+    setupwizard.feature.lifecycle_refactoring=true \
+    setupwizard.feature.notification_refactoring=true \
+    setupwizard.feature.show_pai_screen_in_main_flow.carrier1839=false \
+    setupwizard.feature.show_pixel_tos=true \
+    setupwizard.feature.show_support_link_in_deferred_setup=false \
+    setupwizard.feature.skip_button_use_mobile_data.carrier1839=true
+
 # Set default ringtone, notification and alarm
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.config.alarm_alert=Fresh_start.ogg \
